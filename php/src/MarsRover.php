13928b768c53eccc70aa1ece557e72790cce225b
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2024-03-08 18:40:18 +0100

    green: given_M_command_return_1_0_N

--- php/src/MarsRover.php
+++ php/src/MarsRover.php
@@ -6,6 +6,9 @@ class MarsRover
 {
     public function execute(string $command): string
     {
+        if ($command === 'M') {
+            return '1:0:N';
+        }
         return '0:0:N';
     }
 }
